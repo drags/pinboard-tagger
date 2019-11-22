5e3a817c3362161d4bba8a1f600c9663b006cba7
Tim Sogard <src@timsogard.com>
2019-11-21 22:49:56 -0500

    Loading posts by date, with date picker
    Avoiding the expensive /posts/all endpoint of Pinboard API by loading
    the PostDates and then loading each date as requested. Dates choosable
    from drop down, though not sure this fits any actual use case (other
    than mass importing date of posting doesn't seem like a way users would
    browse to posts. Will probably wind up loading multiple dates in the
    background which will be needed to make the "show untagged posts only"
    option work more smoothly.

--- src/tagger.js
+++ src/tagger.js
@@ -8,36 +8,50 @@ class Tagger extends React.Component {
     super(props);
     this.state = {
       taggerToast: "",
-      filterTags: [],
+      untaggedOnly: false,
       posts: [],
+      postDates: [],
+      tags: [],
       currentPost: 0,
+      currentDate: 0,
       isLoaded: false,
     }
 
+    this.axios = this.props.axios
     this.addTag = this.addTag.bind(this)
     this.deleteTag = this.deleteTag.bind(this)
     this.nextPost = this.nextPost.bind(this)
     this.prevPost = this.prevPost.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
   }
 
-  componentDidMount() {
-    this.getPosts()
-  }
-
-  getPosts() {
-    const axios = this.props.axios
-    axios.get('/posts/get?date=2019-10-25')
-    .then((res) => {
-      console.log(res.data)
-      const newPosts = this.state.posts.concat(res.data);
-      this.setState({
-        posts: newPosts,
-        isLoaded: true,
-      })
+  async componentDidMount() {
+    const postDates = await this.getPostDates()
+    postDates.data.reverse()
+    const posts = await this.getPosts(postDates.data[0].Date)
+    this.setState({
+      postDates: postDates.data,
+      posts: posts.data,
+      isLoaded: true,
     })
+    //this.getTags()
+  }
+
+  getPostDates() {
+    return this.axios.get('/posts/dates')
     .catch((error) => {
       this.setState({
-        taggerToast: "Error from server: " + error.message + ". Try again later.",
+        taggerToast: "Error fetching post dates: " + error.message,
+      })
+    })
+  }
+
+  getPosts(date) {
+    console.log("Getting posts with date", date)
+    return this.axios.get('/posts/get?date=' + date)
+    .catch((error) => {
+      this.setState({
+        taggerToast: "Error fetching posts: " + error.message,
       });
     });
   }
@@ -91,32 +105,79 @@ class Tagger extends React.Component {
       });
   }
 
-  nextPost() {
+  async nextPost() {
     if (this.state.currentPost < (this.state.posts.length - 1)) {
       this.setState({
         currentPost: this.state.currentPost + 1,
       })
+    } else {
+      let nextDate = this.state.currentDate + 1
+      if (nextDate >= this.state.postDates.length) {
+        return
+      }
+      const posts = await this.getPosts(this.state.postDates[nextDate].Date)
+      this.setState({
+        posts: posts.data,
+        currentDate: nextDate,
+        currentPost: 0,
+      })
     }
   }
 
-  prevPost() {
+  async prevPost() {
     if (this.state.currentPost > 0) {
       this.setState({
         currentPost: this.state.currentPost - 1,
       })
+    } else {
+      let prevDate = this.state.currentDate - 1
+      if (prevDate < 0) {
+        return
+      }
+      const posts = await this.getPosts(this.state.postDates[prevDate].Date)
+      this.setState({
+        posts: posts.data,
+        currentDate: prevDate,
+        currentPost: posts.data.length - 1,
+      })
     }
+
+  }
+
+  renderDateOptions() {
+    const dateOptions = this.state.postDates.map((pd, i) => {
+      return(<option key={i} value={i}>{pd.Date}</option>)
+    })
+    return dateOptions
+  }
+
+  async handleDateChange(event) {
+    console.log("handling date change", event.target.value)
+    const dateIndex = event.target.value
+    const posts = await this.getPosts(this.state.postDates[dateIndex].Date)
+    this.setState({
+      posts: posts.data,
+      currentDate: dateIndex,
+      currentPost: 0,
+    })
   }
 
   render() {
     if (this.state.isLoaded) {
       return(
         <div id="tagger">
+          Posts from: <select name="dateSelect" value={this.state.currentDate} onChange={this.handleDateChange}>
+            {this.renderDateOptions()}
+          </select>
           <Post
             post={this.state.posts[this.state.currentPost]}
             deleteTag={this.deleteTag}
             addTag={this.addTag}
           />
         <br />
+        <div>
+          <span className="tagger-toast">{this.state.taggerToast}</span>
+        </div>
         <br />
           <Controls
             nextPost={this.nextPost}
