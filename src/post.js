18c26bad4cd9e93d0d0fbfb1e70165d008f5bbb3
Tim Sogard <src@timsogard.com>
2019-11-21 22:48:59 -0500

    Open URLs in new tab

--- src/post.js
+++ src/post.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Tag from './tag.js';
 
 function renderTags(props) {
-  const tagReturn = []
+  let tagReturn = []
   if (props.post.Tags !== null) {
-    props.post.Tags.map((t) => {
-      return tagReturn.push(<Tag key={t} tag={t} deleteTag={props.deleteTag}/>)
+    tagReturn = props.post.Tags.map((t) => {
+      return(<Tag key={t} tag={t} deleteTag={props.deleteTag}/>)
     })
   }
   // TODO: add new tag input
@@ -16,7 +16,7 @@ function Post(props) {
   return(
     <div className="post">
       <h3>{props.post.Description}</h3>
-      <p><a href={props.post.Url}>{props.post.Url}</a></p>
+      <p><a href={props.post.Url} target="_blank" rel="noopener noreferrer">{props.post.Url}</a></p>
       <p className="post-description">{props.post.Extended}</p>
       <p>Tags: {renderTags(props)}</p>
     </div>
